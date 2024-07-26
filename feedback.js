67871e5f0344f67b5b031fd528cc2419008c86d2
JJPmeyer <79543090+JJPmeyer@users.noreply.github.com>
2024-07-26 16:12:22 +0100

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -1,20 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var feedbackButton = document.getElementById('feedback-button')
-  var feedbackPopup = document.getElementById('feedback-popup')
-  var closePopup = document.getElementById('close-popup')
+document.addEventListener('DOMContentLoaded', function() {
+    var feedbackButton = document.getElementById('feedback-button');
+    var feedbackPopup = document.getElementById('feedback-popup');
+    var closePopup = document.getElementById('close-popup');
 
-  feedbackButton.addEventListener('click', function () {
-    feedbackPopup.classList.remove('hidden')
-  })
+    // Show the popup
+    feedbackButton.addEventListener('click', function() {
+        feedbackPopup.classList.remove('hidden');
+    });
 
-  closePopup.addEventListener('click', function () {
-    feedbackPopup.classList.add('hidden')
-  })
+    // Hide the popup
+    closePopup.addEventListener('click', function() {
+        feedbackPopup.classList.add('hidden');
+    });
 
-  // Close popup if user clicks outside of the popup content
-  window.addEventListener('click', function (event) {
-    if (event.target === feedbackPopup) {
-      feedbackPopup.classList.add('hidden')
-    }
-  })
-})
+    // Close the popup if user clicks outside of the popup content
+    window.addEventListener('click', function(event) {
+        if (event.target === feedbackPopup) {
+            feedbackPopup.classList.add('hidden');
+        }
+    });
+});
