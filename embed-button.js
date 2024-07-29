80af41839db0329ca01dc8a8749ccc9a8c5f862d
JJPmeyer <79543090+JJPmeyer@users.noreply.github.com>
2024-07-29 10:49:53 +0100

    Add files via upload

---
+++ embed-button.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var feedbackButton = document.getElementById('feedback-button')
+  var feedbackPopup = document.getElementById('feedback-popup')
+  var closePopup = document.getElementById('close-popup')
+
+  // Show the popup
+  feedbackButton.addEventListener('click', function () {
+    feedbackPopup.classList.remove('hidden')
+  })
+
+  // Hide the popup
+  closePopup.addEventListener('click', function () {
+    feedbackPopup.classList.add('hidden')
+  })
+
+  // Close the popup if user clicks outside of the popup content
+  window.addEventListener('click', function (event) {
+    if (event.target === feedbackPopup) {
+      feedbackPopup.classList.add('hidden')
+    }
+  })
+})
